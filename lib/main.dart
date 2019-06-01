539b364372769d4c0ba302fef09577ef4b31ded1
RP <rapaat@gmail.com>
2019-06-01 13:27:08 +0300

    Added firebase_ui dependency and login screen wip

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:nw_station/pages/DevicesListPage.dart';
 import 'package:nw_station/pages/LocationListPage.dart';
 import 'package:nw_station/pages/MetersListPage.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_ui/flutter_firebase_ui.dart';
+// import 'package:firebase_ui/l10n/localization.dart';
 
 void main() => runApp(MyApp());
 
@@ -47,6 +51,11 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  StreamSubscription<FirebaseUser> _listener;
+
+  FirebaseUser _currentUser;
+
   PageController _pageController;
   int _selectedPage = 0;
   int _counter = 0;
@@ -56,11 +65,13 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
     super.initState();
+    _checkCurrentUser();
     _pageController = new PageController();
   }
 
   @override
   void dispose() {
+    _listener.cancel();
     super.dispose();
     _pageController.dispose();
   }
@@ -92,45 +103,81 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: new PageView(
-        children: [
-          new LocationListPage(),
-          new MetersListPage(),
-          new DevicesListPage(),
+   if (_currentUser == null) {
+      return new SignInScreen(
+        title: "Demo",
+        header: new Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16.0),
+          child: new Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: new Text("Demo"),
+          ),
+        ),
+        showBar: true,
+        padding: const EdgeInsets.fromLTRB(8, 5, 8, 5),
+        avoidBottomInset: true,
+        color: Color(0xFF363636),
+        providers: [
+          ProvidersTypes.google,
+          //ProvidersTypes.facebook,
+          //ProvidersTypes.twitter,
+          ProvidersTypes.email
         ],
-        onPageChanged: _onPageBarTapped,
-        controller: _pageController,
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.my_location),
-            title: Text('Locations'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.power_input),
-            title: Text('Meters'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.devices),
-            title: Text('Devices'),
-          ),
-        ],
-        currentIndex: _selectedPage,
-        selectedItemColor: Colors.amber[800],
-        onTap: _onPageBarTapped,
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        twitterConsumerKey: "",
+        twitterConsumerSecret: "",
+      );
+    } else {
+      return Scaffold(
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(widget.title),
+        ),
+        body: new PageView(
+          children: [
+            new LocationListPage(),
+            new MetersListPage(),
+            new DevicesListPage(),
+          ],
+          onPageChanged: _onPageBarTapped,
+          controller: _pageController,
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.my_location),
+              title: Text('Locations'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.power_input),
+              title: Text('Meters'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.devices),
+              title: Text('Devices'),
+            ),
+          ],
+          currentIndex: _selectedPage,
+          selectedItemColor: Colors.amber[800],
+          onTap: _onPageBarTapped,
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _incrementCounter,
+          tooltip: 'Increment',
+          child: Icon(Icons.add),
+        ), // This trailing comma makes auto-formatting nicer for build methods.
+      );
+    }
   }
+
+void _checkCurrentUser() async {
+    _currentUser = await _auth.currentUser();
+    _currentUser?.getIdToken(refresh: true);
+
+    _listener = _auth.onAuthStateChanged.listen((FirebaseUser user) {
+      setState(() {
+        _currentUser = user;
+      });
+    });
+  }  
 }
